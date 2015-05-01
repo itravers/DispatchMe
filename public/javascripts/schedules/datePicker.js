784e4c09799118672ad186468db24630dc5deacd
Isaac Assegai <isaac.a.travers@gmail.com>
2015-04-30 17:14:19 -0700

    datepicker disables on input array now

--- public/javascripts/schedules/datePicker.js
+++ public/javascripts/schedules/datePicker.js
@@ -1,12 +1,32 @@
-$(function() {
-	var array = ["2015-04-25","2015-04-27","2015-04-30"]
 
-	$('#datePicker').datepicker({
-	    beforeShowDay: function(date){
-	        var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
-	        return [ array.indexOf(string) == -1 ];
-	    }
+	var app = angular.module("timePickerApp", []); 
+	var timePickerScope;
+
+	//ANGULAR STUFF=====================================================
+	app.controller('timePickerCtrl', function($scope, $http) {
+		$scope.pickedDate = "2015-04-29";
+	      $scope.deactivatedDays = deactivatedDays;
+		deactivateDays();
+		
+		function deactivateDays(){
+			$('#datePicker').datepicker({
+			    beforeShowDay: function(date){
+			    	var array = $scope.deactivatedDays;
+			        var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+			        return [ array.indexOf(string) == -1 ];
+			    }
+			});
+
+			
+		}
 	});
+	
+$(function($scope) {
+	
+	
 });
+	
+
+
 
 
--- routes/schedules.js
+++ routes/schedules.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * This function will query the database for a list of deactivated days.
+ * @returns {Array} The list of deactivated days.
+ */
+function getDeactivatedDays(){
+	var deactivatedDays = ["2015-04-25","2015-04-27","2015-04-30"];
+	return deactivatedDays;
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.render('scheduleTest', { title: 'Schedule Tests' });
+	//var deactivatedDays = ["2015-04-25","2015-04-27","2015-04-30"];
+	var deactivatedDays = getDeactivatedDays();
+
+	
+	res.render('scheduleTest', { title: 'Scheduling Tests', 
+		   "deactivatedDays" : deactivatedDays
+		   });
 });
+
+
 
 module.exports = router;
