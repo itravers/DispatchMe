42ac8ec1bf87fa73cba9b12ca674b51b7302112f
Isaac Assegai <isaac.a.travers@gmail.com>
2015-04-30 17:18:58 -0700

    refacting

--- public/javascripts/schedules/timePicker.js
+++ public/javascripts/schedules/timePicker.js
@@ -1,6 +1,30 @@
 /**
  * New node file
+
  */
+
+
+var app = angular.module("timePickerApp", []); 
+var timePickerScope;
+
+//ANGULAR STUFF=====================================================
+app.controller('timePickerCtrl', function($scope, $http) {
+	$scope.pickedDate = "2015-04-29";
+    $scope.deactivatedDays = deactivatedDays;
+    deactivateDays();
+		
+	function deactivateDays(){
+		$('#datePicker').datepicker({
+		    beforeShowDay: function(date){
+		    	var array = $scope.deactivatedDays;
+			    var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+			    return [ array.indexOf(string) == -1 ];
+			}
+		});
+	}
+	
+});
+	
 $(function() {
 	$('#timePicker').timepicker({
         showPeriod: true,
