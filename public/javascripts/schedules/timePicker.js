25dd9c16b1df8e6a6b4b3b28ec1cd6858859dc6e
Isaac Assegai <isaac.a.travers@gmail.com>
2015-04-30 17:21:10 -0700

    working on hours disableing

--- public/javascripts/schedules/timePicker.js
+++ public/javascripts/schedules/timePicker.js
@@ -12,6 +12,7 @@ app.controller('timePickerCtrl', function($scope, $http) {
 	$scope.pickedDate = "2015-04-29";
     $scope.deactivatedDays = deactivatedDays;
     deactivateDays();
+    deactivateHours();
 		
 	function deactivateDays(){
 		$('#datePicker').datepicker({
@@ -23,23 +24,21 @@ app.controller('timePickerCtrl', function($scope, $http) {
 		});
 	}
 	
-});
+	function deactivateHours(){
+		$('#timePicker').timepicker({
+	        showPeriod: true,
+	        onHourShow: function(hour){
+	        	if ((hour > 20) || (hour < 6)) {
+	                return false; // not valid
+	            }
+	            return true; // valid
+	        },
+	        onMinuteShow: function(hour, minute){
+	        	if ((hour == 20) && (minute >= 30)) { return false; } // not valid
+	            if ((hour == 6) && (minute < 30)) { return false; }   // not valid
+	            return true;  // valid
+	        }
+	    });
+	}
 	
-$(function() {
-	$('#timePicker').timepicker({
-        showPeriod: true,
-        onHourShow: OnHourShowCallback,
-        onMinuteShow: OnMinuteShowCallback
-    });
-function OnHourShowCallback(hour) {
-    if ((hour > 20) || (hour < 6)) {
-        return false; // not valid
-    }
-    return true; // valid
-}
-function OnMinuteShowCallback(hour, minute) {
-    if ((hour == 20) && (minute >= 30)) { return false; } // not valid
-    if ((hour == 6) && (minute < 30)) { return false; }   // not valid
-    return true;  // valid
-}
 });
