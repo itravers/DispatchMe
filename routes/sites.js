9d47261e3c9bff1ec6c080614536c94b17c6fbfe
Isaac Assegai <isaac.a.travers@gmail.com>
2015-05-12 14:54:27 -0700

    user now has the ability to edit any site.

--- routes/sites.js
+++ routes/sites.js
@@ -130,6 +130,7 @@ module.exports = function(app, passport) {
         newSite.name = req.body.siteName;
         newSite.email = req.body.email;
         newSite.templateFile = req.body.templateFile;
+        newSite.thumbnail = req.body.thumbnail;
         newSite.owner = owner._id;
         newSite.formElements = [MainTitle, TagLine, TwitterLink, GithubLink, FacebookLink, ServicesHeading, ServicesSubheading, ServicesParagraph, GoogleLink, LinkedinLink];
         newSite.save(function(err) {// save the new site to the database.
@@ -190,6 +191,143 @@ module.exports = function(app, passport) {
       res.status('200').send(html);
     });
  */
+  });
+  
+  app.post('/site/edit/:siteName', function(req, res){
+    var siteName = req.params.siteName;
+    var regexSiteName = new RegExp(["^",siteName,"$"].join(""),"i"); //ignore capitalization
+    var formElements = [];
+    
+    
+    var MainTitle        = new FormElement();
+    MainTitle.name = "MainTitle";
+    MainTitle.value = req.body.MainTitle;
+    MainTitle.type = "text";
+    MainTitle.label = "Main Title";
+    MainTitle.explaination = "The Main Title/Header of your new page.";
+    formElements.push(MainTitle);
+    
+    var TagLine        = new FormElement();
+    TagLine.name = "TagLine";
+    TagLine.value = req.body.TagLine;
+    TagLine.type = "text";
+    TagLine.label = "Tag Line";
+    TagLine.explaination = "The Tag Line of your new page.";
+    formElements.push(TagLine);
+    
+    var TwitterLink        = new FormElement();
+    TwitterLink.name = "TwitterLink";
+    TwitterLink.value = req.body.TwitterLink;
+    TwitterLink.type = "text";
+    TwitterLink.label = "Twitter Link";
+    TwitterLink.explaination = "A Link to your twitter page.";
+    formElements.push(TwitterLink);
+    
+    var GithubLink        = new FormElement();
+    GithubLink.name = "GithubLink";
+    GithubLink.value = req.body.GithubLink;
+    GithubLink.type = "text";
+    GithubLink.label = "Github Link";
+    GithubLink.explaination = "A Link to your Github page.";
+    formElements.push(GithubLink);
+    
+    var FacebookLink        = new FormElement();
+    FacebookLink.name = "FacebookLink";
+    FacebookLink.value = req.body.FacebookLink;
+    FacebookLink.type = "text";
+    FacebookLink.label = "Facebook Link";
+    FacebookLink.explaination = "A Link to your Facebook page.";
+    formElements.push(FacebookLink);
+    
+    var ServicesHeading        = new FormElement();
+    ServicesHeading.name = "ServicesHeading";
+    ServicesHeading.value = req.body.ServicesHeading;
+    ServicesHeading.type = "text";
+    ServicesHeading.label = "Services Heading";
+    ServicesHeading.explaination = "The Heading to your 'Service' Section.";
+    formElements.push(ServicesHeading);
+    
+    var ServicesSubheading        = new FormElement();
+    ServicesSubheading.name = "ServicesSubheading";
+    ServicesSubheading.value = req.body.ServicesSubheading;
+    ServicesSubheading.type = "text";
+    ServicesSubheading.label = "Services Subheading";
+    ServicesSubheading.explaination = "The Subheading to your 'Service' Section.";
+    formElements.push(ServicesSubheading);
+    
+    var ServicesParagraph        = new FormElement();
+    ServicesParagraph.name = "ServicesParagraph";
+    ServicesParagraph.value = req.body.ServicesParagraph;
+    ServicesParagraph.type = "text";
+    ServicesParagraph.label = "Services Paragraph";
+    ServicesParagraph.explaination = "A short paragraph explaining your services.";
+    formElements.push(ServicesParagraph);
+    
+    var GoogleLink        = new FormElement();
+    GoogleLink.name = "GoogleLink";
+    GoogleLink.value = req.body.GoogleLink;
+    GoogleLink.type = "text";
+    GoogleLink.label = "Google Link";
+    GoogleLink.explaination = "A Link to your Google page.";
+    formElements.push(GoogleLink);
+    
+    var LinkedinLink        = new FormElement();
+    LinkedinLink.name = "LinkedinLink";
+    LinkedinLink.value = req.body.LinkedinLink;
+    LinkedinLink.type = "text";
+    LinkedinLink.label = "Linked-In Link";
+    LinkedinLink.explaination = "A Link to your Linked-In page.";
+    formElements.push(LinkedinLink);
+    
+    
+    
+    
+    req.body.formElements = formElements;
+    Site.findOneAndUpdate({name:regexSiteName}, req.body, function (err, site) {
+      //res.send(site);
+      res.redirect('/site/'+siteName);
+    });
+   // res.send({message : "editing Site " + siteName, body: req.body});
+  });
+  
+  app.get('/site/edit/:siteName', function(req, res){
+    var siteName = req.params.siteName;
+    var regexSiteName = new RegExp(["^",siteName,"$"].join(""),"i"); //ignore capitalization
+    ProtoSite.find({}, {}, function(err, sites) {
+      var errors = [];
+      if (err){ // if there are any errors, return the error to createSite.jade to be displayed to user
+        errors.push(err);
+        res.send({message: errors,
+                    csrfToken: req.csrfToken()}
+        );
+      }
+      if (!sites){ //No site was found by this name, lets create one.
+        res.send({message: "no proto sites found"});
+        
+      }else{ // proto sites were found, now we want to find *this site*
+        //start of site finder
+        Site.findOne({name: siteName}, {}, function(err, site) {
+          var errors = [];
+          if (err){ // if there are any errors, return the error to createSite.jade to be displayed to user
+            errors.push(err);
+            res.send({message: errors,
+                        csrfToken: req.csrfToken()}
+            );
+          }
+          if (!site){ //No site was found by this name, lets create one.
+            res.send({message: "this site not found"});
+            
+          }else{ // proto sites were found, now we want to find *this site*
+            res.render('editSite.jade', {csrfToken: req.csrfToken(),
+                                         thisSite: site,
+                                         sites : sites});
+          } 
+        });
+        //end of site finder
+      }
+      
+        
+    });
   });
   
   // INDIVIDUAL SITES =========================
