8dce68dd00b3e5fa63155e2909e48290381ab6f3
Isaac Assegai <isaac.a.travers@gmail.com>
2015-05-12 02:41:49 -0700

    createSite form appears to correctly create a site.

--- routes/sites.js
+++ routes/sites.js
@@ -42,29 +42,9 @@ module.exports = function(app, passport) {
   app.post('/site/create', function(req, res){
     var siteName = req.body.siteName;
     var tagLine = req.body.tagLine;
-    if(req.body.facebookShare == 'on'){
-      facebookShare = true;
-    }else{
-      facebookShare = false;
-    }
-    if(req.body.Facebook == 'on'){
-      facebookLogin = true;
-    }else{
-      facebookLogin = false;
-    }
-    if(req.body.Twitter == 'on'){
-      twitterLogin = true;
-    }else{
-      twitterLogin = false;
-    }
-    if(req.body.Google == 'on'){
-      googleLogin = true;
-    }else{
-      googleLogin = false;
-    }
     var regexSiteName = new RegExp(["^",siteName,"$"].join(""),"i"); //ignore capitalization
     var owner = req.user;
-    ProtoSite.findOne({ 'name' :  regexSiteName }, "name configCategories", function(err, site) {
+    Site.findOne({ 'name' :  regexSiteName }, "name configCategories", function(err, site) {
       var errors = [];
       if (err){ // if there are any errors, return the error to createSite.jade to be displayed to user
         errors.push(err);
@@ -75,92 +55,83 @@ module.exports = function(app, passport) {
       }
       if (!site){ //No site was found by this name, lets create one.
         console.log("User " + owner + " is creating new site " + siteName);
-        var newSite            = new ProtoSite();
+        var newSite            = new Site();
         
-        var formElement        = new FormElement();
-        formElement.name = "MainTitle";
-        formElement.value = siteName;
-        formElement.type = "text";
-        formElement.label = "Main Title";
-        formElement.explaination = "The Main Title/Header of your new page.";
+        var MainTitle        = new FormElement();
+        MainTitle.name = "MainTitle";
+        MainTitle.value = req.body.MainTitle;
+        MainTitle.type = "text";
+        MainTitle.label = "Main Title";
+        MainTitle.explaination = "The Main Title/Header of your new page.";
         
-        var formElement2        = new FormElement();
-        formElement2.name = "TagLine";
-        formElement2.value = tagLine;
-        formElement2.type = "text";
-        formElement2.label = "Tag Line";
-        formElement2.explaination = "The Tag Line of your new page.";
+        var TagLine        = new FormElement();
+        TagLine.name = "TagLine";
+        TagLine.value = req.body.TagLine;
+        TagLine.type = "text";
+        TagLine.label = "Tag Line";
+        TagLine.explaination = "The Tag Line of your new page.";
         
-        var formElement3        = new FormElement();
-        formElement3.name = "TwitterLink";
-        formElement3.value = "https://twitter.com/OurAutoDidact";
-        formElement3.type = "text";
-        formElement3.label = "Twitter Link";
-        formElement3.explaination = "A Link to your twitter page.";
+        var TwitterLink        = new FormElement();
+        TwitterLink.name = "TwitterLink";
+        TwitterLink.value = req.body.TwitterLink;
+        TwitterLink.type = "text";
+        TwitterLink.label = "Twitter Link";
+        TwitterLink.explaination = "A Link to your twitter page.";
         
-        var formElement4        = new FormElement();
-        formElement4.name = "GithubLink";
-        formElement4.value = "https://github.com/chicosystem";
-        formElement4.type = "text";
-        formElement4.label = "Github Link";
-        formElement4.explaination = "A Link to your Github page.";
+        var GithubLink        = new FormElement();
+        GithubLink.name = "GithubLink";
+        GithubLink.value = req.body.GithubLink;
+        GithubLink.type = "text";
+        GithubLink.label = "Github Link";
+        GithubLink.explaination = "A Link to your Github page.";
         
-        var formElement5        = new FormElement();
-        formElement5.name = "FacebookLink";
-        formElement5.value = "https://facebook.com/itravers";
-        formElement5.type = "text";
-        formElement5.label = "Facebook Link";
-        formElement5.explaination = "A Link to your Facebook page.";
+        var FacebookLink        = new FormElement();
+        FacebookLink.name = "FacebookLink";
+        FacebookLink.value = req.body.FacebookLink;
+        FacebookLink.type = "text";
+        FacebookLink.label = "Facebook Link";
+        FacebookLink.explaination = "A Link to your Facebook page.";
         
-        var formElement6        = new FormElement();
-        formElement6.name = "ServicesHeading";
-        formElement6.value = "Free Contact Pages for";
-        formElement6.type = "text";
-        formElement6.label = "Services Heading";
-        formElement6.explaination = "The Heading to your 'Service' Section.";
+        var ServicesHeading        = new FormElement();
+        ServicesHeading.name = "ServicesHeading";
+        ServicesHeading.value = req.body.ServicesHeading;
+        ServicesHeading.type = "text";
+        ServicesHeading.label = "Services Heading";
+        ServicesHeading.explaination = "The Heading to your 'Service' Section.";
         
-        var formElement7        = new FormElement();
-        formElement7.name = "ServicesSubheading";
-        formElement7.value = "Your Business";
-        formElement7.type = "text";
-        formElement7.label = "Services Subheading";
-        formElement7.explaination = "The Subheading to your 'Service' Section.";
+        var ServicesSubheading        = new FormElement();
+        ServicesSubheading.name = "ServicesSubheading";
+        ServicesSubheading.value = req.body.ServicesSubheading;
+        ServicesSubheading.type = "text";
+        ServicesSubheading.label = "Services Subheading";
+        ServicesSubheading.explaination = "The Subheading to your 'Service' Section.";
         
-        var formElement8        = new FormElement();
-        formElement8.name = "ServicesParagraph";
-        formElement8.value = "We Offer a free professional grade contact page for your small business or organizations. Or, perhaps you would like a simpler way to keep in touch with the grandkids.";
-        formElement8.type = "text";
-        formElement8.label = "Services Paragraph";
-        formElement8.explaination = "A short paragraph explaining your services.";
+        var ServicesParagraph        = new FormElement();
+        ServicesParagraph.name = "ServicesParagraph";
+        ServicesParagraph.value = req.body.ServicesParagraph;
+        ServicesParagraph.type = "text";
+        ServicesParagraph.label = "Services Paragraph";
+        ServicesParagraph.explaination = "A short paragraph explaining your services.";
         
-        var formElement9        = new FormElement();
-        formElement9.name = "GoogleLink";
-        formElement9.value = "https://google.com/itravers";
-        formElement9.type = "text";
-        formElement9.label = "Google Link";
-        formElement9.explaination = "A Link to your Google page.";
+        var GoogleLink        = new FormElement();
+        GoogleLink.name = "GoogleLink";
+        GoogleLink.value = req.body.GoogleLink;
+        GoogleLink.type = "text";
+        GoogleLink.label = "Google Link";
+        GoogleLink.explaination = "A Link to your Google page.";
         
-        var formElement10        = new FormElement();
-        formElement10.name = "LinkedinLink";
-        formElement10.value = "https://linkedin.com/itravers";
-        formElement10.type = "text";
-        formElement10.label = "Linked-In Link";
-        formElement10.explaination = "A Link to your Linked-In page.";
+        var LinkedinLink        = new FormElement();
+        LinkedinLink.name = "LinkedinLink";
+        LinkedinLink.value = req.body.LinkedinLink;
+        LinkedinLink.type = "text";
+        LinkedinLink.label = "Linked-In Link";
+        LinkedinLink.explaination = "A Link to your Linked-In page.";
         
-        newSite.name = siteName;
-        newSite.email = "isaac.a.travers@gmail.com";
-        newSite.templateFile = 'gray-scale.jade';
+        newSite.name = req.body.siteName;
+        newSite.email = req.body.email;
+        newSite.templateFile = req.body.templateFile;
         newSite.owners = [owner._id];
-        newSite.formElements = [formElement, formElement2, formElement3, formElement4, formElement5, formElement6, formElement7, formElement8, formElement9, formElement10];
-        newSite.configCategories = [{name: "AvailableLoginServices",
-                                     configs: [{name: "Facebook", value: facebookLogin},
-                                               {name: "DispatchMyself", value: true},
-                                               {name: "Twitter", value: twitterLogin},
-                                               {name: "Google", value: googleLogin}]
-                                    },
-                                    {name: "AvailableSocialServices",
-                                      configs: [{name: "Facebook", value: facebookShare}]
-                                     }];
+        newSite.formElements = [MainTitle, TagLine, TwitterLink, GithubLink, FacebookLink, ServicesHeading, ServicesSubheading, ServicesParagraph, GoogleLink, LinkedinLink];
         newSite.save(function(err) {// save the new site to the database.
             if (err){//error adding new site
               console.log("error saving new user - database indexes?" + err);
